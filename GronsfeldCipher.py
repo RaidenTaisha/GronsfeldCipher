9aa1a57f3682b98c00c7aad7cb7ff57ebf82e0e9
andrey <andreymydelec@gmail.com>
2021-03-13 12:46:05 +0700

    Update repo

--- GronsfeldCipher.py
+++ GronsfeldCipher.py
@@ -1,11 +1,20 @@
 import string
+string.digits + string.ascii_letters + string.punctuation + string.digits
+alph = string.ascii_uppercase
 
-alphupper = string.ascii_uppercase  #большие буквы
-alphlower = string.ascii_lowercase  #маленькие буквы
-msg = input()
-key = input()
-while ( len(msg)-len(key) < 0 ):
-    buff = key[len(key)-1]
-    key.replace(buff, "")
 
-print(key)
+def algo(text, k, op):
+    k *= len(text) // len(k) + 1
+    return ''.join(alph[alph.index(j) + int(k[i]) * op] for i, j in enumerate(text))
+
+
+def encrypt(message, key):
+    return algo(message, key, 1)
+
+
+def decrypt(message, key):
+    return algo(message, key, -1)
+
+
+print(encrypt('ANCIENT', '420'))  #шифрование
+print(decrypt('EPCMGNX', '420'))  #расшифровывание
